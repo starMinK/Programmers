962033477352e815440dd07dca5d8595028985f4
starMinK <rbals040329@gmail.com>
2022-11-11 22:28:46 +0900

    11-11
    머쓱이보다 키 큰 사람
    중복된 숫자 개수
    옹알이

---
+++ BasicRun.java
@@ -0,0 +1,49 @@
+public class BasicRun {
+    public static void main(String[] args) {
+        /*
+        //프로그래머스 Lv.0 각도기
+        int angle = 90;
+        int answer = basic.SolutionAngle.solutionAngle(angle);
+        System.out.println(answer);
+        */
+
+        /*
+        //프로그래머스 Lv.0 배열의 평균값
+        double answer = basic.SolutionAverage.solutionAverage(new int[]{89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99});
+        System.out.println(answer);
+        */
+
+        /*
+        //프로그래머스 Lv.0 양꼬치
+        int answer = basic.SolutionLambSkewers.solutionLambSkewers(64, 6);
+        System.out.println(answer);
+        */
+
+        /*
+        //Programmers Lv.0 두 수의 나눗셈
+        int answer = basic.SolutionDivision.solutionDivision(1, 16);
+        System.out.println(answer);
+        */
+
+        /*
+        //Programmers Lv.0 머쓱이보다 키 큰 사람
+        int[] array = {180, 120, 140};
+        int answer = basic.SolutionShiny.solutionShiny(array, 190);
+        System.out.println(answer);
+        */
+
+        /*
+        //Programmers Lv.0 중복된 숫자 개수
+        int[] array = {1, 1, 2, 3, 4, 5};
+        int answer = basic.SolutionOverlap.solutionOverlap(array, 1);
+        System.out.println(answer);
+        */
+
+        /*
+        //Programmers Lv.0 옹알이
+        String[] array = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+        int answer = basic.SolutionBabbling.solutionBabbling(array);
+        System.out.println(answer);
+        */
+    }
+}
